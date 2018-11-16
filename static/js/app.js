dea49dba940cf538819f8ee2ad24134ceb6b8265
kychien <41803198+kychien@users.noreply.github.com>
2018-11-16 15:53:51 -0500

    181116: js population of table data

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,12 @@
 // from data.js
 // var tableData = data;
 
+var tbody = d3.select("tbody");
+
+data.forEach(set => {
+    var row = tbody.append("tr");
+    Object.values(set).forEach(value => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
